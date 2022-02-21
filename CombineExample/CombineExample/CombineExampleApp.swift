244daca01d54601241135fa242686ba5bb1fde46
Daesiker <eotlr680@naver.com>
2022-02-21 14:36:13 +0900

    first Practice

---
+++ CombineExample/CombineExample/CombineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExampleApp.swift
+//  CombineExample
+//
+//  Created by Jun on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CombineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
