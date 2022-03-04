e06d31cc7887084580de47cdd7e84f18a350a835
Daesiker <eotlr680@naver.com>
2022-03-04 15:17:47 +0900

    Complete ChapterOne

--- CombineExample/CombineExample/chapterOne.swift
+++ CombineExample/CombineExample/chapterOne.swift
@@ -11,17 +11,58 @@ import Combine
 struct chapterOne: View {
     @StateObject private var mealGenerator = MealGeneration()
     
-    var body: some View {
-        VStack {
-            Button("Get Random Meal") {
-                
-            }
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(16)
+    var actionButton: some View {
+        Button("Get Random Meal") {
+            mealGenerator.fetchRandomMeal()
         }
-        
+        .foregroundColor(.white)
+        .padding()
+        .background(Color.blue)
+        .cornerRadius(16)
+        .onAppear {
+            mealGenerator.fetchRandomMeal()
+        }
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                actionButton
+                if let name = mealGenerator.currentMeal?.name {
+                    Text(name)
+                        .font(.largeTitle)
+                }
+                AsyncImageView(urlString: $mealGenerator.currentImageURLString)
+                
+                if let ingredients = mealGenerator.currentMeal?.ingredients {
+                    HStack {
+                        Text("Ingredients")
+                            .font(.title2)
+                        Spacer()
+                    }
+                    
+                    ForEach(ingredients, id:\.self) { ingredient in
+                        HStack {
+                            Text(ingredient.name + " - " + ingredient.measure)
+                            Spacer()
+                            
+                        }
+                    }
+                }
+                
+                if let instructions = mealGenerator.currentMeal?.instructions {
+                    
+                    HStack {
+                        Text("Instructions")
+                            .font(.title2)
+                        Spacer()
+                    }
+                    
+                    Text(instructions)
+                }
+                
+            }.padding()
+        }
     }
 }
 
@@ -65,7 +106,7 @@ extension Meal {
     }
 }
 
-struct Ingredient: Decodable {
+struct Ingredient: Decodable, Hashable {
     let name:String
     let measure:String
 }
@@ -73,18 +114,75 @@ struct Ingredient: Decodable {
 final class MealGeneration: ObservableObject {
     
     @Published var currentMeal: Meal?
+    @Published var currentImageURLString:String?
     private var cancellable:AnyCancellable?
     
     func fetchRandomMeal() {
-        URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.themealdb.com/api/json/v1/1/random.php")!)
+        cancellable = URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.themealdb.com/api/json/v1/1/random.php")!)
             .receive(on: DispatchQueue.main)
             .sink { _ in
-        } receiveValue: { data, _ in
-            if let mealData = try? JSONDecoder().decode(MealData.self, from: data) {
-                self.currentMeal = mealData.meals.first
+            } receiveValue: { data, _ in
+                if let mealData = try? JSONDecoder().decode(MealData.self, from: data) {
+                    self.currentMeal = mealData.meals.first
+                    self.currentImageURLString = mealData.meals.first?.imageUrlString
+                }
+            }
+        
+    }
+    
+}
+
+struct AsyncImageView: View {
+    
+    @StateObject private var imageLoader = ImageLoader()
+    @Binding var urlString:String?
+    
+    init(urlString: Binding<String?>) {
+        self._urlString = urlString
+    }
+    
+    var image: some View {
+        Group {
+            if let image = imageLoader.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(16)
+            } else {
+                Text("No image")
             }
         }
-
     }
+    
+    var body: some View {
+        
+        image.onChange(of: urlString) { newValue in
+            if let urlString = urlString,
+               let url = URL(string: urlString) {
+                imageLoader.url = url
+                imageLoader.load()
+            }
+        }
+        
+        
+    }
+    
+    
+}
+
+final class ImageLoader: ObservableObject {
+    
+    @Published var image: UIImage?
+    var url: URL?
+    
+    func load() {
+        guard let url = url else { return }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$image)
+    }
+    
     
 }
