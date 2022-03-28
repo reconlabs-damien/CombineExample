5ecddbced0751f2af93336ce479a4607597af8ee
Daesiker <eotlr680@naver.com>
2022-03-28 13:57:05 +0900

    ChapterTen(2)

--- CombineExample/CombineExample/ChapterTen.swift
+++ CombineExample/CombineExample/ChapterTen.swift
@@ -14,7 +14,7 @@ struct MasterView: View {
                 ChapterTen()
                 CardListView()
                 BalanceView()
-                Spacer()
+                TransferMoneyView()
             }.padding(25)
         }
     }
@@ -150,4 +150,45 @@ struct CardView: View {
         .cornerRadius(10)
         .frame(width: 100, height: 150)
     }
+}
+
+struct TransferMoneyView: View {
+    
+    var headerView: some View {
+        HStack {
+            Text("Transfer Money")
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Spacer()
+        }
+        
+    }
+    
+    var body: some View {
+        VStack {
+            headerView
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
+                    ForEach(1..<5) { num in
+                        Image("user\(num)")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(10)
+                    }
+                    
+                    Button {}
+                label: {
+                    Image(systemName: "plus")
+                        .imageScale(.small)
+                        .foregroundColor(.black)
+                }
+                .frame(width: 50, height: 50)
+                .background(Color.primaryYellow)
+                .cornerRadius(5)
+                }
+            }
+        }
+    }
+    
 }
