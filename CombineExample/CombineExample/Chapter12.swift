3e8f8eb9eaed166614d6f915787c691d0a6f3393
Daesiker <eotlr680@naver.com>
2022-04-01 15:25:59 +0900

    Chapter12(2)

--- CombineExample/CombineExample/Chapter12.swift
+++ CombineExample/CombineExample/Chapter12.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import SystemConfiguration
 
 struct Chapter12: View {
     
     @State private var searchText = ""
+    
+    let inProgressCard = Chapter12Cell.init(iconName: "moon", title: "The Silent Night Vibes", subTitle: "2/4 Session left", percentageComplete: 75)
+    
+    let recommendedCards:[Chapter12Cell] = [
+        .init(iconName: "face.smiling", title: "Happiness and Joyful", subTitle: "4 Sessions", percentageComplete: nil),
+        .init(iconName: "heart", title: "Lovely and Vibes", subTitle: "5 Sessions", percentageComplete: nil)
+    ]
     
     init() {
         UINavigationBar.appearance()
@@ -48,6 +56,38 @@ struct Chapter12: View {
             .padding(.horizontal, 20)
     }
     
+    
+    @ViewBuilder
+    var inProgressSectionView: some View {
+        HStack {
+            Text("In Progress")
+                .font(.custom("Avenir-Heavy", size: 18))
+            Spacer()
+        }
+        Chapter12SubView(card: inProgressCard)
+        
+    }
+    
+    @ViewBuilder
+    var recommendedSectionView: some View {
+        HStack {
+            Text("Recommended")
+                .font(.custom("Avenir-Heavy", size: 18))
+            Spacer()
+            
+            Image(systemName: "arrow.right")
+                .foregroundColor(Color(.systemBlue))
+        }
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(recommendedCards, id:\.self) { card in
+                    Chapter12SubView(card: card)
+                        .frame(width: 200)
+                }
+            }
+        }
+    }
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading, spacing: 10) {
@@ -57,6 +97,8 @@ struct Chapter12: View {
                     .font(.custom("Avenir-Medium", size: 18))
                     .foregroundColor(Color(.systemGray))
                 searchView
+                inProgressSectionView
+                recommendedSectionView
             }.padding(24)
         }
         .background(Color(.systemGroupedBackground).ignoresSafeArea())
@@ -79,5 +121,78 @@ struct Chapter12_Previews: PreviewProvider {
             Chapter12()
         }
         
+    }
+}
+
+struct Chapter12Cell:Hashable {
+    let iconName: String
+    let title: String
+    let subTitle: String
+    let percentageComplete: Double?
+    var percentageText: String? {
+        guard let percentageComplete = percentageComplete else {
+            return nil
+        }
+        
+        return "\(percentageComplete)%"
+        
+    }
+}
+
+struct Chapter12SubView: View {
+    
+    let card:Chapter12Cell
+    
+    var body: some View {
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 10).fill(Color.white)
+            
+            VStack(alignment: .leading, spacing: 24) {
+                HStack {
+                    Image(systemName: card.iconName)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(8)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color(.systemBlue).opacity(0.1)))
+                    Spacer()
+                }
+                Text(card.title)
+                    .font(.custom("Avenir-Heavy", size: 22))
+                Text(card.subTitle)
+                    .modifier(Chapter12TextStyle())
+                if let percentageText = card.percentageText {
+                    HStack {
+                        Text(percentageText)
+                            .modifier(Chapter12TextStyle())
+                        Spacer()
+                        ProgressView(value: card.percentageComplete, total: 100)
+                            .progressViewStyle(MeditationProgressViewStyle())
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct Chapter12TextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        return content
+            .font(.custom("Avenir-Medium", size: 18))
+            .foregroundColor(Color(.systemGray))
+    }
+}
+
+struct MeditationProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(.systemGroupedBackground))
+                .frame(height: 10)
+            
+            ProgressView(configuration)
+                .accentColor(Color(.systemBlue))
+                .scaleEffect(x: 1, y: 2.5)
+        }
     }
 }
